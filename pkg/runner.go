32de623b5ace7a3b05ce18bf70de30f76967f9d1
韩数 <33194175+hanshuaikang@users.noreply.github.com>
2024-07-23 23:41:15 +0800

    feat: 新增 config 配置 & review 过程调整为并发执行 (#1)
    * feat: 新增 config 配置 & review 过程调整为并发执行
    
    * feat: 支持设置最大并发数控制

--- pkg/runner.go
+++ pkg/runner.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/google/go-github/v62/github"
 	"os"
+	"strings"
+	"sync"
 )
 
 type Runner interface {
@@ -17,12 +19,15 @@ type CodeReviewRunner struct {
 	gptCli    Cli
 }
 
-func NewCodeReviewRunner(config *Config, repoCli Repo, gptCli Cli) Runner {
+func NewCodeReviewRunner(config *Config, repoCli Repo, gptCli Cli) (Runner, error) {
+	if err := ValidateConfig(*config); err != nil {
+		return nil, err
+	}
 	return &CodeReviewRunner{
 		githubCli: repoCli,
 		gptCli:    gptCli,
 		config:    config,
-	}
+	}, nil
 }
 
 func (c *CodeReviewRunner) isInChanged(line int, pos map[int]int) bool {
@@ -41,11 +46,19 @@ func (c *CodeReviewRunner) reviewCommit(ctx context.Context, commit *github.Repo
 	var comments []*github.DraftReviewComment
 	fmt.Fprintf(os.Stdout, "this commit has %d files need to review \n", len(commit.Files))
 	for _, file := range commit.Files {
+		if !isFileSuffixAllowed(*file.Filename, c.config.ReviewSuffixes) {
+			fmt.Fprintf(os.Stdout, "Skipping file: %s. The file does not have an allowed suffix (%v).\n", file, c.config.ReviewSuffixes)
+			continue
+		}
 		pos := parseDiff(*file.Patch)
 		cc, err := c.githubCli.GetCommitFileContent(ctx, *file.Filename, *commit.SHA)
 		if err != nil {
 			fmt.Println("error getting file content:", err)
 			return nil, err
+		}
+		if c.config.MaxLineNum > 0 && len(strings.Split(cc, "\n")) > c.config.MaxLineNum {
+			fmt.Fprintf(os.Stdout, "file[%s] line num is too large, skip\n", *file.Filename)
+			continue
 		}
 		content := buildParam(c.config.Prompt, cc)
 		reviewedReplyContent, err := c.gptCli.Send(ctx, content)
@@ -85,37 +98,89 @@ func (c *CodeReviewRunner) isHaveCodeReviewRecord(ctx context.Context, commit *g
 
 }
 
-func (c *CodeReviewRunner) RunCodeReview(ctx context.Context) error {
-
+func (c *CodeReviewRunner) getReadyCommits(ctx context.Context) ([]*github.RepositoryCommit, error) {
 	pendingCommits, err := c.githubCli.GetPendingReviewCommits()
 	if err != nil {
-		return err
+		return nil, err
 	}
+
+	if c.config.MaxCommitNum > 0 && len(pendingCommits) > c.config.MaxCommitNum {
+		fmt.Fprintf(os.Stdout, "no pending commits need to review\n")
+		return nil, fmt.Errorf("no pending commits need to review")
+	}
+
+	var alreadyCommits []*github.RepositoryCommit
 
 	for _, commit := range pendingCommits {
 		if c.isHaveCodeReviewRecord(ctx, commit) {
 			fmt.Fprintf(os.Stdout, "commit[%s]: %s  already reviewed\n", *commit.SHA, *commit.Commit.Message)
 			continue
 		}
-		fmt.Fprintf(os.Stdout, "now reviewing commit[%s]: %s\n", *commit.SHA, *commit.Commit.Message)
-		comments, err := c.reviewCommit(ctx, commit)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error reviewing commit, commit[%s]: %s, err :%s\n", *commit.SHA, *commit.Commit.Message, err)
-			return err
-		}
-		fmt.Fprintf(os.Stdout, "found %d comments in commit[%s]: %s \n", len(comments), *commit.SHA, *commit.Commit.Message)
-		if comments == nil {
-			continue
-		}
-		// review end , submit the code review with this commit
-		err = c.githubCli.SubmitCodeReview(ctx, *commit, comments)
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "submit code review error, commit[%s]: %s, err: %s\n", *commit.SHA, *commit.Commit.Message, err)
-			return err
-		}
-		fmt.Fprintf(os.Stdout, "submit code review success, commit[%s]: %s, comment nums: %d\n", *commit.SHA, *commit.Commit.Message, len(comments))
+		alreadyCommits = append(alreadyCommits, commit)
 	}
 
+	return alreadyCommits, nil
+
+}
+
+func (c *CodeReviewRunner) RunCodeReview(ctx context.Context) error {
+
+	alreadyCommits, err := c.getReadyCommits(ctx)
+	if err != nil {
+		return err
+	}
+
+	if len(alreadyCommits) == 0 {
+		fmt.Fprintf(os.Stdout, "No commits are ready for review.\n")
+		return nil
+	}
+
+	concurrency := 1
+	if c.config.MaxConcurrency > 0 {
+		concurrency = c.config.MaxConcurrency
+	}
+	if concurrency > len(alreadyCommits) {
+		concurrency = len(alreadyCommits)
+	}
+
+	var wg sync.WaitGroup
+	semaphore := make(chan struct{}, concurrency)
+
+	for _, commit := range alreadyCommits {
+		if c.isHaveCodeReviewRecord(ctx, commit) {
+			fmt.Fprintf(os.Stdout, "commit[%s]: %s  already reviewed\n", *commit.SHA, *commit.Commit.Message)
+			continue
+		}
+		if c.config.MaxFileNum > 0 && len(commit.Files) > c.config.MaxFileNum {
+			fmt.Fprintf(os.Stdout, "commit[%s]: %s  file num is too large, skip\n", *commit.SHA, *commit.Commit.Message)
+			continue
+		}
+		wg.Add(1)
+		semaphore <- struct{}{} // Acquire semaphore
+		go func(commit *github.RepositoryCommit) {
+			defer wg.Done()
+			defer func() {
+				<-semaphore
+			}()
+			fmt.Fprintf(os.Stdout, "now reviewing commit[%s]: %s\n", *commit.SHA, *commit.Commit.Message)
+			comments, err := c.reviewCommit(ctx, commit)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error reviewing commit, commit[%s]: %s, err :%s\n", *commit.SHA, *commit.Commit.Message, err)
+				return
+			}
+			fmt.Fprintf(os.Stdout, "found %d comments in commit[%s]: %s \n", len(comments), *commit.SHA, *commit.Commit.Message)
+			if comments == nil {
+				return
+			}
+			// review end, submit the code review with this commit
+			err = c.githubCli.SubmitCodeReview(ctx, *commit, comments)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "submit code review error, commit[%s]: %s, err: %s\n", *commit.SHA, *commit.Commit.Message, err)
+				return
+			}
+			fmt.Fprintf(os.Stdout, "submit code review success, commit[%s]: %s, comment nums: %d\n", *commit.SHA, *commit.Commit.Message, len(comments))
+		}(commit)
+	}
+	wg.Wait()
 	return nil
 }
--- readme.md
+++ readme.md
@@ -1,16 +1,26 @@
-# ChatGpt Code Review
-[English Document](https://github.com/hanshuaikang/chatgpt-codereview/blob/main/readme_en.md)
 
-ChatGpt Code Review 是一个用Golang 编写的 AI code Review 工具，它可以利用AI自动某个Github PR
-的 Review ，并 comment 到对应的代码段下。效果如下:
+<h1 align="center"> 🧐 ChatGpt Code Review</h1>
+<p align="center">
+    <em>ChatGpt Code Review 是一个用Golang 编写的 AI code Review 工具，它可以利用AI自动某个Github PR
+的 Review ，并 comment 到对应的代码段下。</em>
+</p>
+
 
 ![img.png](docs/imgs/img.png)
 
+[English Document](https://github.com/hanshuaikang/chatgpt-codereview/blob/main/readme_en.md)
 ## Feature:
 - PR 下的 commit 只会 Review 一次
 - 基于全文件的Review 比基于 diff 的质量更高
 
 ## Usage
+
+## 配置说明
+- `max_file_num`: 如果本次需要 review 的 commit 的文件数大于该配置，则跳过该 commit 的 review， 为 0 则不限制。
+- `max_line_num`: 如果待 review 的文件行数大于该配置, 则跳过该 文件 的 review， 为 0 则不限制。
+- `max_commit_num`: 如果待 review 的 pr 的 commits 数量大于该配置，则跳过该 pr 的 review， 为 0 则不限制。
+- `review_suffixes`: 当指定了 review_suffixes 时, 将只会 review 为 review_suffixes 后缀的文件，为 [] 则不限制。
+- `max_concurrency`: 同时 review 的最大并发数, 不设置时为 1
 
 有两种方式使用 ChatGpt Code Review
 
@@ -26,7 +36,15 @@ owner: "repo owner"
 repo: "repo name"
 api_key: "openai key"
 pr: pr Id
+max_file_num: 10
+max_line_num: 1000
+max_commit_num: 100
+max_concurrency: 3
+review_suffixes:
+  - ".go"
 ```
+
+
 
 ```bash
 go install github.com/hanshuaikang/chatgpt-codereview@latest
@@ -60,18 +78,28 @@ func main() {
 		Prompt: "",
 		ApiKey: "",
 		Token:  "",
+		MaxFileNum: 10,
+		MaxLineNum: 1000,
+		MaxCommitNum: 100,
+		MaxConcurrency: 3,
+		ReviewSuffixes: []string{".go"},
 	}
 	
     defaultGptCli := chatgpt.NewGptClient(config)
     githubCli := github.NewGithubCli(config.Token, config.Owner, config.Repo, config.Pr)
-    runner := pkg.NewCodeReviewRunner(&config, githubCli, defaultGptCli)
-
+    runner, err := pkg.NewCodeReviewRunner(&config, githubCli, defaultGptCli)
+	if err != nil {
+		os.Exit(1)
+	}
 	ctx := context.Background()
-	err := runner.RunCodeReview(ctx)
+	err = runner.RunCodeReview(ctx)
 	if err != nil {
 		os.Exit(1)
 	}
 }
 ```
 
+## 附录
 
+实现思路
+![](./docs/imgs/img_1.png)
